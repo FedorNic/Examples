fb8762bfc90cd77fd75b949d99c31c8cf65160ef
FedorNic <dmitrievfedor@yandex.ru>
2022-02-02 11:39:23 +0300

    dots1

--- Example_10_Dots/Program.cs
+++ Example_10_Dots/Program.cs
@@ -1,4 +1,7 @@
 ﻿Console.Clear();
+//Console.SetCursorPosition(10, 4);
+//Console.WriteLine("+");
+
 int xa = 40, ya = 1,
     xb = 1, yb = 30,
     xc = 80, yc = 30;
@@ -12,30 +15,33 @@ Console.WriteLine("+");
 Console.SetCursorPosition(xc, yc);
 Console.WriteLine("+");
 
-int x = xa, y = ya;
 
-int Count = 0;
+int x = xa, y = xb;
 
-while (Count < 100)
+int count = 0;
+
+while (count < 10000)
 {
-    int what = new Random().Next(0, 3); // 0,1,2
+    int what = new Random().Next(0, 3); // [0;3) 0 1 2
     if (what == 0)
     {
         x = (x + xa) / 2;
-        x = (y + ya) / 2;
+        y = (y + ya) / 2;
     }
+
     if (what == 1)
     {
         x = (x + xb) / 2;
-        x = (y + yb) / 2;
+        y = (y + yb) / 2;
     }
+
     if (what == 2)
     {
         x = (x + xc) / 2;
-        x = (y + yc) / 2;
+        y = (y + yc) / 2;
     }
 
     Console.SetCursorPosition(x, y);
     Console.WriteLine("+");
-    Count = Count + 1;
+    count++;
 }
