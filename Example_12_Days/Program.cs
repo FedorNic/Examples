c06914531d0c72d69b3d413cb02d2dc511e9b1a9
FedorNic <dmitrievfedor@yandex.ru>
2022-02-02 22:13:43 +0300

    Final 2

---
+++ Example_12_Days/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+string day = Console.ReadLine();
+int Day = Convert.ToInt32(day);
+if(Day == 1)
+Console.WriteLine("Понедельник");
+if(Day == 2)
+Console.WriteLine("Вторник");
+if(Day == 3)
+Console.WriteLine("Среда");
+if(Day == 4)
+Console.WriteLine("Четверг");
+if(Day == 5)
+Console.WriteLine("Пятница");
+if(Day == 6)
+Console.WriteLine("Суббота");
+if(Day == 7)
+Console.WriteLine("Воскресенье");
---
+++ Example_12_Days_Array/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Введите номер дня недели: ");
+string day = Console.ReadLine();
+int Day = Convert.ToInt32(day);
+int Array = [1, 2, 3, 4, 5, 6, 7];
+string Array1 = [Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье];
+numbers[index]
