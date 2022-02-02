9a104cf3d75374778f98e4736de92d3bf66b5244
FedorNic <dmitrievfedor@yandex.ru>
2022-02-02 20:21:28 +0300

    New sum

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,5 +1,9 @@
-﻿int numberA = new Random ().Next(1, 10); // 1,2,3...9
-Console.WriteLine(numberA);
-int numberB = new Random ().Next(1, 10);
-Console.WriteLine(numberB);
-Console.WriteLine(numberA+numberB);
+﻿Console.WriteLine("Введите первое число: ");
+string numA = Console.ReadLine();
+int numerA = Convert.ToInt32(numA);
+Console.WriteLine("Введите второе число: ");
+string numB = Console.ReadLine();
+int numerB = Convert.ToInt32(numB);
+int result = numerA + numerB;
+Console.WriteLine("Полученная сумма= ");
+Console.Write(result);
