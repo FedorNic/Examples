3a61123437a2402863691ace0d153a6c4ca014c0
FedorNic <dmitrievfedor@yandex.ru>
2022-02-03 22:05:12 +0300

    Final 4

---
+++ Example_14/Program.cs
@@ -0,0 +1,7 @@
+﻿// Вывести в терминал последнюю цифру трехзначного числа
+Console.WriteLine("Введите трехзначный номер : ");
+string num = Console.ReadLine();
+int number = Convert.ToInt32(num);
+
+int a1 = number % 10;
+Console.WriteLine("Последний номер: "+a1);
---
+++ Example_17/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число. Проверить кратно ли оно 7 и 23
+Console.WriteLine("Введите число: ");
+string num1 = Console.ReadLine();
+int number1 = Convert.ToInt32(num1);
+
+if (number1 %7 == 0 && number1 %23 == 0) 
+
+Console.WriteLine("Кратно 7 и 23");
+
+else
+    Console.WriteLine("Не кратно");
